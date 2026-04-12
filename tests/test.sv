bc751df06a1e1394208ac3763d30bcc7d195a40e
hxxdev <hxx.dev@gmail.com>
2026-04-13 01:56:23 +0900

    change test verilog code.

--- tests/test.sv
+++ tests/test.sv
@@ -1,5 +1,5 @@
 
-memory(address, data_in, data_out, read_write, chip_en);
+module memory(address, data_in, data_out, read_write, chip_en);
 
 input wire [7:0] address, data_in;
 output reg [7:0] data_out;
