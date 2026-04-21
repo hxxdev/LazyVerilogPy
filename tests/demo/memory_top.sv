98ab6bb34e89136fc529181d5e029de9c57e5925
hxxdev <hxx.dev@gmail.com>
2026-04-22 00:21:31 +0900

    update demo

--- tests/demo/memory_top.sv
+++ tests/demo/memory_top.sv
@@ -2,14 +2,31 @@
 
 `define WIDTH 32
 
-function logic [3:0] sum(input i_clk);
-    return a + b;
+function logic [3:0] sum(input i_a, input i_b);
+    return i_a + i_b;
 endfunction
+
+task add_numbers(input int a, input int b, output int result);
+    result = a + b;
+endtask
 
 module memory_top(i_clk);
     input i_clk;
-    logic           [`WIDTH-1:0]            data            ;
-    logic           [2:0]                   a               , b               , c               ;
+    logic           [`WIDTH-1:0]            data                    ;
+    // a, b, c
+    logic           [2:0]                   a                       , b /* eho */             , c                       ;
+    //dd
+    /* ehlo */  // a, b, c
+    //
+    //
+    //
+    //
+    logic           [7:0]                   data_out                ;
+    // b
+    /* ehlo */
+    logic           [2:0]                   d                       ;
+
+    assign d = a;
 
     memory u_memory (
         .i_clk                  (i_clk                  ),
@@ -25,7 +42,8 @@ module memory_top(i_clk);
 
     always_comb begin
         a    = 3;
-        c    = sum(a + b);
+        c    = sum(.i_a(3), .i_b(i_b));
+        c    = add_numbers(a, b, result);
     end
 
 endmodule
